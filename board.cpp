e1baa461541c2f273a8c6a39a1e9bb1fc66df067
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-08-13 15:05:18 -0400

    add headers

---
+++ board.cpp
@@ -0,0 +1,17 @@
+#define FMT_HEADER_ONLY
+
+#include <vector>
+#include <string>
+//#include <ranges>
+#include <functional>
+#include <iostream>
+
+//#include <valarray>
+#include <algorithm>
+#include <iterator>
+
+#include <assert.h>
+#include "include/fmt/core.h"
+#include "include/fmt/format.h"
+
+using namespace std;
