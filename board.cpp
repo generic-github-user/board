17a9196a70d3a568bcb4a92472bbed10d2767219
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-08-13 22:40:14 -0400

    add comments describing Point class data members and methods

--- board.cpp
+++ board.cpp
@@ -33,22 +33,22 @@ class Vec;
 // coordinate system; sometimes also used to represent an offset or motion
 // through space (i.e., a delta)
 class Point {
-		vector<int> position;
+		vector<int> position; // Coordinates of the point
 
 		public:
 
-		Point(vector<int> pos);
-		Point();
-		Point(int x, int y);
+		Point(vector<int> pos); // Takes a vector and returns a new point
+		Point(); // Default point constructor (i.e., (0, 0))
+		Point(int x, int y); // Takes an x and y coordinate and returns a new point
 
-		int& operator[] (int index);
-		bool operator== (Point& other);
-		Point operator+ (Point other);
-		Point operator- (Point other);
-		Point clone();
-		Point abs();
-		string tostring();
-		vector<int> vec();
+		int& operator[] (int index); // Returns the component along the specified axis
+		bool operator== (Point& other); // Test point equality (by value)
+		Point operator+ (Point other); // Elementwise addition of points
+		Point operator- (Point other); // Elementwise subtraction of points
+		Point clone(); // Returns a copy of this point
+		Point abs(); // Applies the absolute value function to each axis
+		string tostring(); // Generate a short string representation of the point
+		vector<int> vec(); // Convert this point to a vector
 };
 
 // An "agent" or independent actor participating in a game via moves
