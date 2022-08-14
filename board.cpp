0b2ac4e3a72e8ac2a71ff3e0a570a3a15c24c332
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-08-13 23:11:13 -0400

    comment Piece class

--- board.cpp
+++ board.cpp
@@ -71,20 +71,20 @@ class Player {
 class Piece {
 		public:
 
-		string name;
-		string symbol;
-		Point position;
+		string name; // The piece's name (specifically, its type)
+		string symbol; // A symbol (ideally a single character) used to represent this piece, e.g. when rendering ASCII representations of game boards
+		Point position; // Location of the piece on the board
 		vector<Move*> history;
 
-		Board* board;
-		Player* player;
+		Board* board; // The game board this piece "exists" within
+		Player* player; // A reference to the piece's owner or the player associated with it in some sense
 
-		Piece(string n, string s, Point pos);
-		Piece(string n, string s, Point pos, Player* p);
-		vector<Move*> traverse(Point pos, Point delta, vector<Move*> movelist);
-		vector<Move*> moves();
-		string tostring();
-		Piece clone();
+		Piece(string n, string s, Point pos); // Construct a piece not associated with a player
+		Piece(string n, string s, Point pos, Player* p); // Construct a piece associated with a player
+		vector<Move*> traverse(Point pos, Point delta, vector<Move*> movelist); // Attempts a "virtual move" in the direction specified by `delta` until a stopping condition is met; each intermediate position is added to `movelist`
+		vector<Move*> moves(); // Returns a list of (references to) available moves for this piece
+		string tostring(); // Returns a string summarizing an instance of this class
+		Piece clone(); // Returns a copy of this instance
 };
 
 // A space in which a game takes place; wraps a set of pieces and some topology
